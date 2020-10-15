9ab5762b9021e7313db19baf0b81f60b0d3aec87
Stephen Peters <7speter@gmail.com>
2020-10-15 13:12:47 -0400

    Adds Bootstrap and implements navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewNavbarComponent } from './view-navbar/view-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewNavbarComponent
   ],
   imports: [
     BrowserModule,
