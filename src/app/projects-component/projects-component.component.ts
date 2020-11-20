ff05c7c8035d5993d4910d12cd8809f1c553eca4
Stephen Peters <7speter@gmail.com>
2020-11-20 15:08:39 -0500

    Begins to shape static data for databinding in projects page

--- src/app/projects-component/projects-component.component.ts
+++ src/app/projects-component/projects-component.component.ts
@@ -6,7 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects-component.component.css']
 })
 export class ProjectsComponent implements OnInit {
+  pageTitle: string = 'My Work';
 
+  projects: any[] = [
+    {
+      projectId: 0,
+      projectName: 'Random Quote Generator',
+      image: 'https://via.placeholder.com/1280x960',
+      technologies: 'jQuery, jQueryUI, Bootstrap, Random Quote API, HTML, CSS',
+      description: 'A random quote generator that a user can use to get new quotes with the clicke of a button.',
+      githubLink: 'github.com',
+      appLink: 'github.com'
+    },
+    {
+      projectId: 1,
+      projectName: 'Random Quote Generator',
+      image: 'https://via.placeholder.com/1280x960',
+      technologies: 'jQuery, jQueryUI, Bootstrap, Random Quote API, HTML, CSS',
+      description: 'A random quote generator that a user can use to get new quotes with the clicke of a button.',
+      githubLink: 'github.com',
+      appLink: 'github.com'
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
