4258f32de8614df8f968011d9e3b56f715898b80
Stephen Peters <7speter@gmail.com>
2020-11-21 15:34:01 -0500

    Getting links to work based on needs

--- src/app/projects-component/projects-component.component.ts
+++ src/app/projects-component/projects-component.component.ts
@@ -12,7 +12,16 @@ export class ProjectsComponent implements OnInit {
     {
       projectId: 0,
       projectName: 'Random Quote Generator',
-      image: 'https://via.placeholder.com/1280x960',
+      imgUrl: 'https://via.placeholder.com/1280x961',
+      technologies: 'jQuery, jQueryUI, Bootstrap, Random Quote API, HTML, CSS',
+      description: 'A random quote generator that a user can use to get new quotes with the clicke of a button.',
+      githubLink: 'https://www.github.com',
+      appLink: 'https://www.github.com'
+    },
+    {
+      projectId: 1,
+      projectName: 'Weather Viewer',
+      imgUrl: 'https://via.placeholder.com/1280x962',
       technologies: 'jQuery, jQueryUI, Bootstrap, Random Quote API, HTML, CSS',
       description: 'A random quote generator that a user can use to get new quotes with the clicke of a button.',
       githubLink: 'github.com',
@@ -21,11 +30,11 @@ export class ProjectsComponent implements OnInit {
     {
       projectId: 1,
       projectName: 'Weather Viewer',
-      image: 'https://via.placeholder.com/1280x960',
+      imgUrl: 'https://via.placeholder.com/1280x963',
       technologies: 'jQuery, jQueryUI, Bootstrap, Random Quote API, HTML, CSS',
       description: 'A random quote generator that a user can use to get new quotes with the clicke of a button.',
-      githubLink: 'github.com',
-      appLink: 'github.com'
+      githubLink: 'http://www.github.com',
+      appLink: 'https://www.github.com'
     }
   ];
   constructor() { }
