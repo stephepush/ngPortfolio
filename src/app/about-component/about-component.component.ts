306e9f54669cb3dc3d2127ba5abe5ffab333451a
Stephen Peters <7speter@gmail.com>
2020-12-11 13:49:42 -0500

    Adds list to about me page

--- src/app/about-component/about-component.component.ts
+++ src/app/about-component/about-component.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-component.component.css']
 })
 export class AboutComponent implements OnInit {
+  pageTitle: string = 'About Me';
+
+  completedTech: any[] = ['HTML/CSS/JavaScript', 'ReactJS', 'BootStrap', 'jQuery', 'Node', 'PostGRES', 'WordPress', 'Git']
+  experimentedTech: any[] = ['Angular', 'PHP', 'MySQL', 'Ruby/Rails', 'Python', 'Django' ]
 
   constructor() { }
 
