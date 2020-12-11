46ca165a440cdd5767e5697167c1e60106d49412
Stephen Peters <7speter@gmail.com>
2020-12-11 15:36:40 -0500

    Adds fontawesome imports to contact-component.ts

--- src/app/contact-component/contact-component.component.ts
+++ src/app/contact-component/contact-component.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faWordpress } from '@fortawesome/free-brands-svg-icons';
+
+//import { } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-contact-component',
@@ -6,6 +12,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-component.component.css']
 })
 export class ContactComponent implements OnInit {
+
+  linkedinIn = faLinkedinIn;
+  github = faGithub;
+  twitter = faTwitter;
+
 
   constructor() { }
 
